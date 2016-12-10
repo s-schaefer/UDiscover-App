53d94c8538c0f22b2ab35449323411d0eab476fc
s-schaefer <ssdragon96@gmail.com>
2016-12-10 12:27:09 -0500

    Add files via upload

---
+++ UDiscoverApp/app/src/main/java/com/myself/serena_pc/udiscoverapp/GetPlainMapActivity.java
@@ -0,0 +1,16 @@
+package com.myself.serena_pc.udiscoverapp;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetPlainMapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_plain_map);
+    }
+
+
+}
